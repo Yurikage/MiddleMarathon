cd67d8833afc7d5dd3ad1a9f5e63e46545d2dc6e
Yura Petrov <pe.ur.ur@gmail.com>
2017-06-14 13:43:09 +0300

    Singleton

--- Patterns/Creational/Singleton.playground/Contents.swift
+++ Patterns/Creational/Singleton.playground/Contents.swift
@@ -1,5 +1,26 @@
-//: Playground - noun: a place where people can play
+/*The singleton pattern ensures that only one object of a particular class is ever created. All further references to objects of the singleton class refer to the same underlying instance. There are very few applications, do not overuse this pattern!*/
 
-import UIKit
+//Example 1
+class SingletonObject {
+    private static let obj: SingletonObject = {
+        return SingletonObject()
+    }()
+    
+    class func singleton() -> SingletonObject {
+        return obj
+    }
+    
+    var tmpProperty: String!
+}
 
-var str = "Hello, playground"
+SingletonObject.singleton().tmpProperty = "Hello 2 You"
+print(SingletonObject.singleton().tmpProperty)
+
+//Example 2
+
+class DeathSuperlaser {
+    static let sharedInstance = DeathSuperlaser()
+    private init() {}
+}
+
+let laser = DeathSuperlaser.sharedInstance
